fc094613f84d2325d38613108d3d75b8dfb17b8e
ttb10912 <ttb10912@TTB10912s-MacBook-Pro.local>
2016-09-14 10:13:43 +0700

    .
    .

--- DemoBird/ViewController.swift
+++ DemoBird/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var bird = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        drawJungle()
+        addBird()
+        flyUpAndDown()
+//        playSong()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func drawJungle()  {
+        let background = UIImageView(image: UIImage(named: "jungle.jpg"))
+        background.frame = self.view.bounds
+        background.contentMode = .ScaleAspectFill
+        self.view.addSubview(background)
+        
+    }
+    func addBird() {
+        bird = UIImageView(frame: CGRectMake(0, 0, 110, 68  ))
+        bird.animationImages = [ UIImage(named:"bird0.png")! ,
+                                 UIImage(named:"bird1.png")! ,
+                                 UIImage(named:"bird2.png")! ,
+                                 UIImage(named:"bird3.png")! ,
+                                 UIImage(named:"bird4.png")! ,
+                                 UIImage(named:"bird5.png")! ]
+        bird.animationRepeatCount = 0
+        bird.animationDuration = 2
+        bird.startAnimating()
+        self.view.addSubview(bird)
+    }
+    func flyUpAndDown() {
+        UIView.animateWithDuration(4, animations: {
+            self.bird.center = CGPointMake(self.view.bounds.size.width , self.view.bounds.size.height )
+            
+            
+            
+        })
+        { (finished) in
+                
+        }
+        
+        
+    }
 }
 
